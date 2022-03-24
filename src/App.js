05daefe35dc8afc04e6d6b4ae90524d6f157a863
vaibhav <vaibhavkrpandey111@gmail.com>
2022-03-24 17:49:34 +0530

    quiz commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useGlobalContext } from './context'
 
+import SetupForm from './SetupForm'
+import Loading from './Loading'
+import Modal from './Modal'
 function App() {
+  const {waiting, loading, questions, index, correct,nextQuestion,checkAnswer} = useGlobalContext()
+  if(waiting){
+    return <SetupForm/>
+  }
+  if(loading) {
+    return <Loading />
+  }
+ const {question, incorrect_answers, correct_answer} = questions[index]
+//console.log(questions[0])
+//  const answers = [...incorrect_answers, correct_answer]
+
+//to setup answer at random position
+let answers = [...incorrect_answers]
+//to get random no. between 0 and 4.
+const tempIndex = Math.floor(Math.random() * 4)
+if(tempIndex === 3){
+  answers.push(correct_answer)
+}
+else {
+  answers.push(answers[tempIndex])
+  answers[tempIndex] = correct_answer
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main>
+      <Modal />
+      <section className='quiz'>
+        <p className='correct-answers'>
+          correct answers : {correct}/{index}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <article className='container'>
+          {/* Since the data contained in question is not in string format but in html format so, we need to use this attribute in order to fetch the question in correct format. */}
+          {/* We shouldn't use this attribute randomly or where we take user input as the user can add malware to the data. */}
+          <h2 dangerouslySetInnerHTML={{__html: question}}/>
+          <div className='btn-container'>
+            {answers.map((answer, index) => {
+              return (
+                <button key = {index} className = 'answer-btn' 
+                onClick={()=> checkAnswer(correct_answer === answer)}
+                dangerouslySetInnerHTML={{__html: answer}} />
+              )
+            })}
+          </div>
+        </article>
+        <button className='next-question' onClick={nextQuestion}>
+          next question
+        </button>
+      </section>
+    </main>
   );
 }
 
-export default App;
+export default App
